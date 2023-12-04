4719136e3bc328db2e932f0bb57e14eaf7f679da
jmainhard <jmainhard@outlook.com>
2023-12-04 11:37:48 -0300

    test(H-2): home is correct for logged user

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -6,4 +6,19 @@ describe("Home", () => {
 			"http://pruebas-soft.s3-website.us-east-2.amazonaws.com/login"
 		);
 	});
+
+	describe("As a logged user", () => {
+		beforeEach(() => {
+			cy.login();
+		});
+
+		it.only("[SUCCESS H-2] Home correct", () => {
+			cy.visit("/");
+
+			cy.window().then((win) => {
+				const userName = JSON.parse(win.localStorage.getItem("user")).user.name;
+				cy.get("h2").should("contain", `Welcome ${userName}`);
+			});
+		});
+	});
 });
