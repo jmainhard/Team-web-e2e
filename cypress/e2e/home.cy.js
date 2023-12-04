dd9d3dac5c832f2eadc654b55640dac970cd0182
jmainhard <jmainhard@outlook.com>
2023-12-04 18:20:08 -0300

    test(h-3): get list of clubs

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,16 +8,32 @@ describe("Home", () => {
 	});
 
 	describe("As a logged user", () => {
+		var token = null;
+
 		beforeEach(() => {
+			// FIXME: We should get the token from the login command
+			// TODO: saveLocalStorage plugin to preserve localStorage between Cypress tests?
 			cy.login();
+			token = cy.window().then((win) => {
+				token = JSON.parse(win.localStorage.getItem("user")).token;
+			});
 		});
 
-		it.only("[SUCCESS H-2] Home correct", () => {
+		it("[SUCCESS H-2] Home correct", () => {
 			cy.visit("/");
-
 			cy.window().then((win) => {
 				const userName = JSON.parse(win.localStorage.getItem("user")).user.name;
 				cy.get("h2").should("contain", `Welcome ${userName}`);
+			});
+		});
+
+		it("[SUCESS H-3] Get list of clubs", () => {
+			cy.visit("/");
+			cy.getClubs(token).then((clubs) => {
+				clubs.forEach((club) => {
+					// Evaluate: What happens if there are a lot of clubs?
+					cy.get(`div[id=${club._id}]`).should("exist");
+				});
 			});
 		});
 	});
